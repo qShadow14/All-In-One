4fc885e20c1ae1b795f555f5d14cf7c4d1fb74f5
qShadow14 <qshadowqshadow13@gmail.com>
2026-01-27 09:18:31 -0800

    Create TopBar-Buttons.lua

---
+++ TopBar-Buttons.lua
@@ -0,0 +1,27 @@
+Window:CreateTopbarButton("Inf", "infinity", function() 
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() 
+end, 990)
+
+Window:CreateTopbarButton("ketamine", "code", function() 
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Ketamine/refs/heads/main/Ketamine.lua"))() 
+end, 990)
+
+Window:CreateTopbarButton("dex", "keyboard", function() 
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))() 
+end, 990)
+
+Window:CreateTopbarButton("starbutton1", "star", function() 
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/qShadow14/Emotes/refs/heads/main/emotes.lua"))()
+end, 990)
+
+Window:CreateTopbarButton("Items", "syringe", function() 
+	loadstring(game:HttpGet("https://pastefy.app/qpbQo0lr/raw"))()
+end, 990)
+
+Window:CreateTopbarButton("starbutton1", "scroll", function() 
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/qShadow14/All-In-One/refs/heads/main/Andrei-Script.lua"))()
+end, 990)
+
+Window:CreateTopbarButton("starbutton1", "scroll", function() 
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/qShadow14/All-In-One/refs/heads/main/Dragos-Script.lua"))()
+end, 990)
