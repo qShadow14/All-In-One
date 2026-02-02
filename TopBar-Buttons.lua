984c8af88f8a0f70ac11d893616c981bbed85d53
qShadow14 <qshadowqshadow13@gmail.com>
2026-02-02 10:20:28 -0800

    Update TopBar-Buttons.lua

--- TopBar-Buttons.lua
+++ TopBar-Buttons.lua
@@ -22,6 +22,6 @@ Window:CreateTopbarButton("starbutton1", "skull", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/qShadow14/All-In-One/refs/heads/main/Game_kick.lua"))()
 end, 990)
 
-Window:CreateTopbarButton("starbutton1", "scroll", function() 
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/qShadow14/All-In-One/refs/heads/main/Dragos-Script.lua"))()
+Window:CreateTopbarButton("starbutton1", "server", function() 
+	loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/a93582389752a21f5d298a76cf600078022f2ee639446ee5ab585274f7ba7bae/download"))()
 end, 990)
