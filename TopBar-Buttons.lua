5c13149efdb7b474707f9f18099d3708238051d1
qShadow14 <qshadowqshadow13@gmail.com>
2026-03-28 00:21:40 +0200

    Update TopBar-Buttons.lua

--- TopBar-Buttons.lua
+++ TopBar-Buttons.lua
@@ -2,20 +2,12 @@ Window:CreateTopbarButton("Inf", "infinity", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() 
 end, 990)
 
-Window:CreateTopbarButton("ketamine", "code", function() 
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Ketamine/refs/heads/main/Ketamine.lua"))() 
-end, 990)
-
 Window:CreateTopbarButton("dex", "keyboard", function() 
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))() 
 end, 990)
 
 Window:CreateTopbarButton("starbutton1", "star", function() 
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/qShadow14/All-In-One/refs/heads/main/Emotes.lua"))()
-end, 990)
-
-Window:CreateTopbarButton("Items", "syringe", function() 
-	loadstring(game:HttpGet("https://pastefy.app/qpbQo0lr/raw"))()
 end, 990)
 
 Window:CreateTopbarButton("starbutton1", "skull", function() 
