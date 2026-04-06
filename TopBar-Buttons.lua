66350e39c5d6cc225b798b0a3c4db74b846b6819
qShadow14 <qshadowqshadow13@gmail.com>
2026-04-06 19:33:41 +0300

    Update TopBar-Buttons.lua

--- TopBar-Buttons.lua
+++ TopBar-Buttons.lua
@@ -10,10 +10,14 @@ Window:CreateTopbarButton("starbutton1", "star", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/qShadow14/All-In-One/refs/heads/main/Emotes.lua"))()
 end, 990)
 
-Window:CreateTopbarButton("starbutton1", "skull", function() 
+Window:CreateTopbarButton("Kick", "skull", function() 
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/qShadow14/All-In-One/refs/heads/main/Game_kick.lua"))()
 end, 990)
 
-Window:CreateTopbarButton("starbutton1", "server", function() 
+Window:CreateTopbarButton("TrafficConeHax", "server", function() 
 	loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/a93582389752a21f5d298a76cf600078022f2ee639446ee5ab585274f7ba7bae/download"))()
+end, 990)
+
+Window:CreateTopbarButton("starbutton1", "car", function() 
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Documantation12/Universal-Vehicle-Script/main/Main.lua'))()
 end, 990)
