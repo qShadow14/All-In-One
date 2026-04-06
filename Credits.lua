7ecfd55984727c7c0a0f9b82859a6f0865c694eb
qShadow14 <qshadowqshadow13@gmail.com>
2026-04-06 17:48:14 +0300

    Create Credits.lua

---
+++ Credits.lua
@@ -0,0 +1,41 @@
+Tab:Paragraph({
+    Title = "👑 Owner",
+    Desc = "[1] qShadow / Vexor ",
+    Color = Color3.fromRGB(255, 80, 255),
+    Locked = false,
+})
+
+Tab:Paragraph({
+    Title = "💻 Developer",
+    Desc = "[1] TeoNair\n[2] H1NEXX\n[3] Dreezy",
+    Color = Color3.fromRGB(80, 255, 160),
+    Locked = false,
+})
+
+Tab:Paragraph({
+    Title = "⚡ Head Admin",
+    Desc = "[1] .ftgs\n[2] CM KALAN",
+    Color = Color3.fromRGB(255, 160, 40),
+    Locked = false,
+})
+
+Tab:Paragraph({
+    Title = "🛡️ Admin",
+    Desc = "[1] d\n[2] SSHRK\n[3] Dinga",
+    Color = Color3.fromRGB(80, 180, 255),
+    Locked = false,
+})
+
+Tab:Paragraph({
+    Title = "🔧 Moderator",
+    Desc = "[1] K I R A \n[2] Hi\n [3] CM KALAN",
+    Color = Color3.fromRGB(255, 220, 80),
+    Locked = false,
+})
+
+Tab:Paragraph({
+    Title = "🎨 Designer",
+    Desc = "[1] BirdBall\n [2] Dreezy\n [3] Teo\n [4]H1NEXX",
+    Color = Color3.fromRGB(80, 180, 255),
+    Locked = false,
+})
