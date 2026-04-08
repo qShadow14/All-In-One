f853fd59c91be36388f096a086bd24803fa19734
qShadow14 <qshadowqshadow13@gmail.com>
2026-04-08 19:36:07 +0300

    Update Cursor_2.lua

--- Cursors/Cursor_2.lua
+++ Cursors/Cursor_2.lua
@@ -9,7 +9,7 @@ local playerGui = player:WaitForChild("PlayerGui")
 --           CURSOR SETTINGS (Edit Here)
 -- =============================================
 
-local CURSOR_IMAGE = "rbxassetid:/99289185289544"
+local CURSOR_IMAGE = "rbxassetid://99289185289544"
 local CURSOR_SIZE  = 20
 
 local COLOR_MODE = "static"
